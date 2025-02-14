03154f21f386be198dda546d60e40541a5ca189f
unrz266 <martin.fischer@fau.de>
2025-02-14 11:03:09 +0100

    double click for pretty print

--- src/components/TheConverter.vue
+++ src/components/TheConverter.vue
@@ -18,6 +18,27 @@ function convert() {
   groovy.value = output;
 }
 
+function pp() {
+  let output = query.value.toString();
+  // replace number literals
+  output = output.replace(/NumberInt\((\d+)\)/gm, '"___NumberInt___$1"');
+  // replace date literals
+  output = output.replace(/ISODate\("(.*)"\)/gm, '"___ISODate___$1"');//'"__ISODate__"$1');
+  let j = JSON.parse(output);
+  query.value = JSON.stringify(j, (k, v) => {
+    if (typeof v === 'string') {
+      if (v.startsWith('___NumberInt___')) {
+        v = 'NumberInt(' + v.substring(15, v.length) + ')';
+      } else if (v.startsWith('___ISODate___')) {
+        v = 'ISODate(' + v.substring(13, v.length) + ')';
+      }
+    }
+    return v
+  }, 2);
+
+  convert();
+}
+
 function syncscroll(event) {
   let st = event.target.scrollTop;
   Array.from(document.getElementsByTagName("textarea")).forEach((el) => {
@@ -28,7 +49,7 @@ function syncscroll(event) {
 
 <template>
   <div id="container">
-    <textarea v-model="query" @scroll="syncscroll($event)" placeholder="JSON" @change="convert()"></textarea>
+    <textarea v-model="query" @dblclick="pp()" @scroll="syncscroll($event)" placeholder="JSON" @change="convert()"></textarea>
     <textarea v-model="groovy" @scroll="syncscroll($event)" placeholder="groovy" readonly></textarea>
   </div>
 </template>
